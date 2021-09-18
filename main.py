21291f49e8e455aee129b4a6bfef42a96b871271
Jason Li <jla690@cs.sfu.ca>
2021-09-17 23:52:37 -0700

    made changes to put into class

--- main.py
+++ main.py
@@ -1,71 +1,72 @@
-def valid(board, randomNum, row, col):
-    row_nums = board[row]
-    if randomNum in row_nums:
-        return False
-    col_nums = []
-    for i in range(9):
-        col_nums.append(board[i][col])
-    if randomNum in col_nums:
-        return False
+class Board:
+    def __init__(self):
+        self.board = [
+            [7, 8, 0, 4, 0, 0, 1, 2, 0],
+            [6, 0, 0, 0, 7, 5, 0, 0, 9],
+            [0, 0, 0, 6, 0, 1, 0, 7, 8],
+            [0, 0, 7, 0, 4, 0, 2, 6, 0],
+            [0, 0, 1, 0, 5, 0, 9, 3, 0],
+            [9, 0, 4, 0, 6, 0, 0, 0, 5],
+            [0, 7, 0, 3, 0, 0, 0, 1, 2],
+            [1, 2, 0, 0, 0, 7, 4, 0, 0],
+            [0, 4, 9, 2, 0, 6, 0, 0, 7]
+        ]
 
-    rowOfSquare = (row // 3) * 3
-    colOfSquare = (col // 3) * 3
+    def valid(self, randomNum, row, col):
+        row_nums = self.board[row]
+        if randomNum in row_nums:
+            return False
+        col_nums = []
+        for i in range(9):
+            col_nums.append(self.board[i][col])
+        if randomNum in col_nums:
+            return False
 
-    for i in range(rowOfSquare, rowOfSquare + 3):
-        for j in range(colOfSquare, colOfSquare + 3):
-            if board[i][j] == randomNum:
-                return False
-    return True
+        rowOfSquare = (row // 3) * 3
+        colOfSquare = (col // 3) * 3
 
-
-def print_board(board):
-    for row in range(9):
-        print("")
-        print("|", end="")
-        for col in range(9):
-            print(board[row][col], end='|')
-    print('\n')
-
-
-def find_empty(board):
-    for row in range(9):
-        for col in range(9):
-            if board[row][col] == 0:
-                return row, col
-    return None, None
-
-
-def solve(board):
-    row, col = find_empty(board)
-    if row is None:
+        for i in range(rowOfSquare, rowOfSquare + 3):
+            for j in range(colOfSquare, colOfSquare + 3):
+                if self.board[i][j] == randomNum:
+                    return False
         return True
 
-    for randomNum in range(1, 10):
-        if valid(board, randomNum, row, col):
-            board[row][col] = randomNum
-            if solve(board):
-                return True
+    def print_board(self):
+        for row in range(9):
+            print("")
+            print("|", end="")
+            for col in range(9):
+                print(self.board[row][col], end='|')
+        print('\n')
 
-        board[row][col] = 0
-    return False
+    def find_empty(self):
+        for row in range(9):
+            for col in range(9):
+                if self.board[row][col] == 0:
+                    return row, col
+        return None, None
+
+    def solve(self):
+        row, col = self.find_empty()
+        if row is None:
+            return True
+
+        for randomNum in range(1, 10):
+            if self.valid(randomNum, row, col):
+                self.board[row][col] = randomNum
+                if self.solve():
+                    return True
+
+            self.board[row][col] = 0
+        return False
 
 
 if __name__ == '__main__':
-    game_board = [
-        [7, 8, 0, 4, 0, 0, 1, 2, 0],
-        [6, 0, 0, 0, 7, 5, 0, 0, 9],
-        [0, 0, 0, 6, 0, 1, 0, 7, 8],
-        [0, 0, 7, 0, 4, 0, 2, 6, 0],
-        [0, 0, 1, 0, 5, 0, 9, 3, 0],
-        [9, 0, 4, 0, 6, 0, 0, 0, 5],
-        [0, 7, 0, 3, 0, 0, 0, 1, 2],
-        [1, 2, 0, 0, 0, 7, 4, 0, 0],
-        [0, 4, 9, 2, 0, 6, 0, 0, 7]
-    ]
+    game_board = Board()
     print("Before:", end="")
-    print_board(game_board)
-    if solve(game_board):
+    game_board.print_board()
+    if game_board.solve():
         print("After:", end="")
-        print_board(game_board)
+        game_board.print_board()
     else:
         print("Not solvable")
